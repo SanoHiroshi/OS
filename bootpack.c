d32f58db68625410d4674a4dba9be4c355c46a9d
SanoHiroshi <jankiryu0711@gmail.com>
2016-12-21 10:22:38 +0900

    Display String with font

--- bootpack.c
+++ bootpack.c
@@ -8,7 +8,9 @@ void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void bookfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-
+void init_screen(char *vram, int x, int y);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void putfont8_asc(char *vram, int xsize, int x, int y, char c, char *s);
 // 色の定義
 #define COL8_000000 0
 #define COL8_FF0000 1
@@ -27,45 +29,26 @@ void bookfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, 
 #define COL8_008484 14
 #define COL8_848484 15
 
-void HariMain(void){
-  char *vram; // BYTE[...]用の番地
-  int xsize, ysize;
+struct BOOTINFO{
+  char cyls, leds, vmode, reserve;
+  short scrnx, scrny;
+  char *vram;
+};
+
+void HariMain(void)
+{
+  struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
+
   init_palette(); // パレットの設定（画面の色を設定すること）
-
-  vram = (char *) 0xa0000; // 番地を代入
-  xsize = 320;
-  ysize = 200;
-
-  bookfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29); //画面の色を1pxずつ塗っていく
-  bookfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 28, xsize - 1, ysize - 28);
-  bookfill8(vram, xsize, COL8_FFFFFF, 0, ysize - 27, xsize - 1, ysize - 27);
-  bookfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 26, xsize - 1, ysize - 1);
-
-  bookfill8(vram, xsize, COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24);
-  bookfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4);
-  bookfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);
-  bookfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
-  bookfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);
-  bookfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
-
-  bookfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
-  bookfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4);
-  bookfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 3, xsize - 4, ysize - 3);
-  bookfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
+  init_screen(binfo->vram, binfo->scrnx, binfo->scrny); // 画面の初期化処理
+  putfont8_asc(binfo->vram, binfo->scrnx, 8, 8, COL8_FFFFFF, "Welcome to"); // 文字を画面に書く
+  putfont8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_FFFFFF, "Sano First OS"); // 文字を画面に書く
+  putfont8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Sano First OS"); // 文字を画面に書く
 
   for (;;) { // 条件を省略したら無限ループ
     io_hlt(); // CPUを停止させて、省電力にする命令
   }
-}
 
-void bookfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1){
-  int x, y;
-  for (y = y0; y <= y1; y++){
-    for (x = x0; x <= x1; x++){
-      vram[y * xsize + x] = c;
-    }
-  }
-  return;
 }
 
 void init_palette(){
@@ -93,6 +76,67 @@ void init_palette(){
   // static char 命令はデータにしか使えないけどDB命令相当
 }
 
+void init_screen(char *vram, int x, int y){
+  bookfill8(vram, x, COL8_008484, 0, 0, x - 1, y - 29); //画面の色を1pxずつ塗っていく
+  bookfill8(vram, x, COL8_C6C6C6, 0, y - 28, x - 1, y - 28);
+  bookfill8(vram, x, COL8_FFFFFF, 0, y - 27, x - 1, y - 27);
+  bookfill8(vram, x, COL8_C6C6C6, 0, y - 26, x - 1, y - 1);
+
+  bookfill8(vram, x, COL8_FFFFFF, 3, y - 24, 59, y - 24);
+  bookfill8(vram, x, COL8_FFFFFF, 2, y - 24, 2, y - 4);
+  bookfill8(vram, x, COL8_848484, 3, y - 4, 59, y - 4);
+  bookfill8(vram, x, COL8_848484, 59, y - 23, 59, y - 5);
+  bookfill8(vram, x, COL8_000000, 2, y - 3, 59, y - 3);
+  bookfill8(vram, x, COL8_000000, 60, y - 24, 60, y - 3);
+
+  bookfill8(vram, x, COL8_848484, x - 47, y - 24, x - 4, y - 24);
+  bookfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y - 4);
+  bookfill8(vram, x, COL8_FFFFFF, x - 47, y - 3, x - 4, y - 3);
+  bookfill8(vram, x, COL8_FFFFFF, x - 3, y - 24, x - 3, y - 3);
+
+  return;
+}
+
+void bookfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1){
+  int x, y;
+  for (y = y0; y <= y1; y++){
+    for (x = x0; x <= x1; x++){
+      vram[y * xsize + x] = c;
+    }
+  }
+  return;
+}
+
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
+{
+  int i;
+  char *p, d; // data
+  for (i = 0; i < 16; i++){
+    p = vram + (y + i) * xsize + x;
+    d = font[i];
+    if ((d & 0x80) != 0){ p[0] = c; }
+    if ((d & 0x40) != 0){ p[1] = c; }
+    if ((d & 0x20) != 0){ p[2] = c; }
+    if ((d & 0x10) != 0){ p[3] = c; }
+    if ((d & 0x08) != 0){ p[4] = c; }
+    if ((d & 0x04) != 0){ p[5] = c; }
+    if ((d & 0x02) != 0){ p[6] = c; }
+    if ((d & 0x01) != 0){ p[7] = c; }
+  }
+  return;
+}
+
+void putfont8_asc(char *vram, int xsize, int x, int y, char c, char *s)
+{
+  extern char hankaku[4096];
+
+  for (; *s != 0x00; s++)
+  {
+    putfont8(vram, xsize, x , y, c, hankaku + *s * 16);
+    x += 8;
+  }
+  return;
+}
 void set_palette(int start, int end, unsigned char *rgb){
   int i, eflags;
   eflags = io_load_eflags(); // 割り込み許可フラグの値を記録する
